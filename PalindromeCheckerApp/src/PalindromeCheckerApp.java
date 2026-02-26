902e97b895581712606912f0f471a07efada6686
yps-coder <yp2988@srmist.edu.in>
2026-02-26 15:36:26 +0530

    [Yashpratapsingh] added [UC2]

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,6 +1,50 @@
+/**
+ * =====================================================
+ * MAIN CLASS - UseCase2PalindromeCheckerApp
+ * =====================================================
+ *
+ * Use Case 2: Hardcoded Palindrome Validation
+ *
+ * Description:
+ * This class demonstrates basic palindrome validation
+ * using a hardcoded string value.
+ *
+ * The application:
+ * - Stores a predefined string
+ * - Compares characters from both ends
+ * - Determines whether the string is a palindrome
+ * - Displays the result on the console
+ *
+ * @author Developer
+ * @version 2.0
+ */
+
 public class PalindromeCheckerApp {
-    public static void main(String[] agrs){
-        System.out.println("welcome to palindrome checker");
-        System.out.println("modify coder");
+
+    /**
+     * Application entry point for UC2.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is not a palindrome.");
+        }
     }
 }
